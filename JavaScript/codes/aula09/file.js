4c297a591603966366c67c816070d89330d96e6e
Eduardo Dias <165398613+blackeduard@users.noreply.github.com>
2024-07-03 11:17:18 -0300

    JavaScript.6

---
+++ JavaScript/codes/aula09/file.js
@@ -0,0 +1,8 @@
+
+var p1 = document.getElementsByTagName('p')[0]
+var mensagem = document.getElementById('msg')
+window.document.write("Está escrito assim: "+ p1.innerHTML)
+mensagem.style.background = 'green'
+mensagem.style.textAlign = 'center'
+mensagem.style.padding = '10px'
+mensagem.style.marginBottom = '16px'
