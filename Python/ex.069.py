6e596a375cbc1dce5110a75610df46a32a82e6cd
Eduardo Dias <165398613+blackeduard@users.noreply.github.com>
2024-08-09 08:55:46 -0300

    Python.6

--- Python/ex.069.py
+++ Python/ex.069.py
@@ -1,6 +1,21 @@
-# Crie um programa que leia a idade e o sexo de várias pessoas. 
-# A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. 
-# ? No final mostre:
-# - Quantas pessoas tem mais de 18 anos;
-# - Quantos homens foram cadastrados;
-# - Quantas mulheres tem menos de 20 anos.
+c = maior = homens = womans = 0
+while True:
+    c += 1
+    print(f'-=-=-=-=-=-=-{c}° Pessoa-=-=-=-=--=-=-')
+    idade = int(input('Idade: '))
+    sexo = input('Sexo [M/F]: ').upper()
+    while sexo != 'M' and sexo != 'F':
+        sexo = input('Sexo [M/F]: ').upper()
+    esc = input('Quer continuar [S/N]? ').upper()
+    # todo Pensamento
+    if idade >= 18:
+        maior += 1
+    if sexo == 'M':
+        homens += 1
+    if sexo == 'F' and idade < 20:
+        womans += 1
+    while esc != 'S' and esc != 'N':
+        esc = input('Quer continuar [S/N]? ').upper()
+    if esc == 'N':
+        break
+print(f'{maior} pessoas tem mais de 18 anos\n{homens} homens foram cadastrados\n{womans} mulheres tem menos de 20 anos')
--- Python/ex.070.py
+++ Python/ex.070.py
@@ -4,3 +4,31 @@
 # - Qual é o total gasto na compra;
 # - Quantos produtos custam mais de R$1000.00;
 # - Qual é o nome do produto mais barato.
+total = c = quantidade = barato = 0
+nome_barato = ''
+frase = 'Loja On-line'
+print(f'{frase:^24}')
+while True:
+    c += 1
+    if c == 1:
+        print(f'-=-=-| {c}° Produto |-=-=-')
+    else:
+        print(f'\n-=-=-| {c}° Produto |-=-=-')
+    nome_produto = input('Nome: ')
+    preco_produto = float(input('Preço: '))
+    esc = input('Quer continuar [S/N]? ').upper()
+    if esc != 'S' and esc != 'N':
+        esc = input('Quer continuar [S/N]? ').upper()
+    # todo Pensamento
+    total += preco_produto
+    if preco_produto > 1000:
+        quantidade += 1
+    if c == 1:
+        nome_barato = nome_produto
+        barato = preco_produto
+    if barato > preco_produto:
+        nome_barato = nome_produto
+        barato = preco_produto
+    if esc == 'N':
+        break
+print(f'O total gasto na compra é de: R${total:.2f}\n{quantidade} produtos custam mais de R$1000.00\n"{nome_barato}" é o produto mais barato')
